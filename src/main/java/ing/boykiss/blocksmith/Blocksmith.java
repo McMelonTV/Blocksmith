bf7d3809882e052d95f9918e750a9ee5cc02c5e5
mel <mcmelon@nodiumhosting.com>
2025-08-15 08:40:56 +0200

    refactor: remove logger

--- src/main/java/ing/boykiss/blocksmith/Blocksmith.java
+++ src/main/java/ing/boykiss/blocksmith/Blocksmith.java
@@ -1,8 +1,5 @@
 package ing.boykiss.blocksmith;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Blocksmith {
-    public static final Logger LOGGER = LoggerFactory.getLogger(Blocksmith.class);
+
 }
