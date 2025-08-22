9b1c9bd9d4da67185f05193a0eba5e82c1704d4d
mel <mcmelon@nodiumhosting.com>
2025-08-22 10:15:26 +0200

    feat: StructureData

--- core/src/main/java/ing/boykiss/blocksmith/structure/StructureData.java
+++ core/src/main/java/ing/boykiss/blocksmith/structure/StructureData.java
@@ -1,13 +1,109 @@
 package ing.boykiss.blocksmith.structure;
 
+import ing.boykiss.blocksmith.block.BlockDef;
+import ing.boykiss.blocksmith.block.BlockRegistry;
 import ing.boykiss.blocksmith.util.Value;
+import net.kyori.adventure.key.Key;
 import net.kyori.adventure.nbt.CompoundBinaryTag;
+import net.minestom.server.coordinate.BlockVec;
+import net.minestom.server.instance.block.Block;
+import org.intellij.lang.annotations.Subst;
+import org.javatuples.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class StructureData {
-    public int[] size; // [x,y,z]
-    public int[][] blocks; // [[x,y,z,blockIndex,stateIndex,nbtIndex],...]
-    public String[] namespacePalette; // ["minecraft","somemod",...]
-    public Value.KeyValue<Value.IntValue>[] blockPalette; // [{"oak_leaves":0},{"someblock":1},...]
-    public Value.KeyValue[][] statePalette; // [[{"waterlogged":true},{"persistent":true}],...]
-    public CompoundBinaryTag[] nbtPalette; // [{__block_data__},...]
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * IMPORTANT: This class is not very efficient for regular usage and should therefore only be used for Serialization/Deserialization.
+ * For other uses please use the Structure class.
+ *
+ * @param size             [x,y,z]
+ * @param blocks           [[x,y,z,blockIndex,stateIndex,nbtIndex],...]
+ * @param namespacePalette ["minecraft","somemod",...]
+ * @param blockPalette     [{"oak_leaves":0},{"someblock":1},...]
+ * @param statePalette     [[{"waterlogged":true},{"persistent":true}],...]
+ * @param nbtPalette       [{__block_data__},...]
+ * @see Structure
+ */
+public record StructureData(
+        int[] size,
+        int[][] blocks,
+        String[] namespacePalette,
+        Value.KeyValue<Value.IntValue>[] blockPalette,
+        Value.KeyValue<?>[][] statePalette,
+        CompoundBinaryTag[] nbtPalette
+) {
+    public static final int BLOCKDATA_INDEX_X = 0;
+    public static final int BLOCKDATA_INDEX_Y = 1;
+    public static final int BLOCKDATA_INDEX_Z = 2;
+    public static final int BLOCKDATA_INDEX_BLOCK = 3;
+    public static final int BLOCKDATA_INDEX_STATE = 4;
+    public static final int BLOCKDATA_INDEX_NBT = 5;
+
+    public @NotNull BlockVec getSize() {
+        return new BlockVec(size[BLOCKDATA_INDEX_X], size[BLOCKDATA_INDEX_Y], size[BLOCKDATA_INDEX_Z]);
+    }
+
+    public @Nullable Block getBlock(@NotNull BlockVec pos, @Nullable BlockRegistry customBlockRegistry) {
+        int[] blockData = getBlockData(pos);
+        if (blockData == null) return null;
+
+        return toBlock(blockData, customBlockRegistry);
+    }
+
+    public @NotNull List<Pair<@NotNull BlockVec, @NotNull Block>> asBlockList(@Nullable BlockRegistry customBlockRegistry) {
+        List<Pair<BlockVec, Block>> list = new ArrayList<>(blocks.length);
+
+        for (int[] blockData : blocks) {
+            Block block = toBlock(blockData, customBlockRegistry);
+            if (block != null) {
+                list.add(Pair.with(getBlockPos(blockData), block));
+            }
+        }
+
+        return list;
+    }
+
+    private @Nullable Block toBlock(int @NotNull [] blockData, @Nullable BlockRegistry customBlockRegistry) {
+        Value.KeyValue<Value.IntValue> blockId = blockPalette[blockData[BLOCKDATA_INDEX_BLOCK]];
+        @Subst("minecraft") String namespace = namespacePalette[blockId.value().value()];
+        @Subst("dirt") String path = blockId.key();
+        Key blockKey = Key.key(namespace, path);
+        Value.KeyValue<?>[] state = statePalette[blockData[BLOCKDATA_INDEX_STATE]];
+        CompoundBinaryTag nbt = nbtPalette[blockData[BLOCKDATA_INDEX_NBT]];
+
+        Map<String, String> stateMap = new HashMap<>();
+        for (Value.KeyValue<?> prop : state) {
+            stateMap.put(prop.key(), prop.value().toString());
+        }
+
+        Block baseBlock = Block.fromKey(blockKey);
+        if (baseBlock == null && customBlockRegistry == null) {
+            return null;
+        } else if (customBlockRegistry != null) {
+            return customBlockRegistry.get(blockKey).map(BlockDef::getBlock).map(b -> b.withProperties(stateMap).withNbt(nbt)).orElse(null);
+        }
+
+        return baseBlock.withProperties(stateMap).withNbt(nbt);
+    }
+
+    private @NotNull BlockVec getBlockPos(int @NotNull [] blockData) {
+        return new BlockVec(blockData[BLOCKDATA_INDEX_X], blockData[BLOCKDATA_INDEX_Y], blockData[BLOCKDATA_INDEX_Z]);
+    }
+
+    private int @Nullable [] getBlockData(@NotNull BlockVec pos) {
+        int[] blockPos = new int[]{pos.blockX(), pos.blockY(), pos.blockZ()};
+        for (int[] blockData : blocks) {
+            if (blockData[BLOCKDATA_INDEX_X] != blockPos[BLOCKDATA_INDEX_X]
+                    || blockData[BLOCKDATA_INDEX_Y] != blockPos[BLOCKDATA_INDEX_Y]
+                    || blockData[BLOCKDATA_INDEX_Z] != blockPos[BLOCKDATA_INDEX_Z])
+                continue;
+            return blockData;
+        }
+        return null;
+    }
 }
--- core/src/main/java/ing/boykiss/blocksmith/util/Value.java
+++ core/src/main/java/ing/boykiss/blocksmith/util/Value.java
@@ -1,17 +1,31 @@
 package ing.boykiss.blocksmith.util;
 
+import org.jetbrains.annotations.NotNull;
+
 public sealed interface Value<T> permits Value.BoolValue, Value.IntValue, Value.StringValue {
     T value();
 
     String toString();
 
     record IntValue(Integer value) implements Value<Integer> {
+        @Override
+        public @NotNull String toString() {
+            return value.toString();
+        }
     }
 
     record BoolValue(Boolean value) implements Value<Boolean> {
+        @Override
+        public @NotNull String toString() {
+            return value.toString();
+        }
     }
 
     record StringValue(String value) implements Value<String> {
+        @Override
+        public @NotNull String toString() {
+            return value;
+        }
     }
 
     record KeyValue<T extends Value>(String key, T value) {
